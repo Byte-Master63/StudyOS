fedb5375c30555ee21d72841439bc3e0bf399df2
Byte-Master63 <thokozanetek@gmail.com>
2026-07-06 19:14:48 +0200

    feat: add static tasks data source

---
+++ src/data/tasks.js
@@ -0,0 +1,5 @@
+export const tasks = [
+  { id: 1, title: "Finish MAT1503 assignment", done: false },
+  { id: 2, title: "Review HCI usability notes", done: false },
+  { id: 3, title: "Submit C++ tax bracket program", done: true },
+];
